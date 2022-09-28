a38e611f61acae9dcd754252fc0734d7619a843f
imswaggyboy <75663923+imswaggyboy@users.noreply.github.com>
2022-09-28 19:22:07 +0530

    Update alphaB_pattern.py

--- alphaB_pattern.py
+++ alphaB_pattern.py
@@ -6,3 +6,12 @@ for row in range(7):
         else: 
             print(end=" ")
     print()
+
+    #'B' shape pattern
+for row in range(7):
+    for col in range(5):
+        if ((row==0 or row==3 or row==6) and col!=4) or ((col==0 or col==4) and (row==1 or row==2 or row==4 or row==5)):
+            print("*",end='')
+        else: 
+            print(end=" ")
+    print()

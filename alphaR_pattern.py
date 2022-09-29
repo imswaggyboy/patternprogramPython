83df930d5aac1bfab9e64d5012cded7c1f6a678d
imswaggyboy <75663923+imswaggyboy@users.noreply.github.com>
2022-09-29 13:43:17 +0530

    'R' shape star pattern python

---
+++ alphaR_pattern.py
@@ -0,0 +1,8 @@
+# 'R' shape star pattern
+for row in range(7):
+    for col in range(5):
+        if ((row==0 or row==3) and col!=4) or ((col==0 or col==4) and (row!=0 and row!=3)):
+            print("*",end="")
+        else:
+            print(end=" ")
+    print()

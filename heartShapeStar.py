374796fa18ef98e945b572c4d4cfb87a89be56f1
imswaggyboy <75663923+imswaggyboy@users.noreply.github.com>
2022-10-03 16:32:07 +0530

    heartShapeStar.py

---
+++ heartShapeStar.py
@@ -0,0 +1,8 @@
+star = input("Enter any single character:")
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print(star,end="")
+        else:
+            print(end=" ")
+    print()

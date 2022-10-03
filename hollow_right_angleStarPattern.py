2cea4f58c762ba60e84636fc4223a9adaf1de135
imswaggyboy <75663923+imswaggyboy@users.noreply.github.com>
2022-10-03 13:25:10 +0530

    #hollow right angle shape as per user input

--- hollow_right_angleStarPattern.py
+++ hollow_right_angleStarPattern.py
@@ -1,3 +1,16 @@
+# hollow right angle shape as per user input 
+n= int(input("Enter the number of rows: "))
+for row in range(n):
+    for col in range(n):
+        if row==0 or col==n-1 or (row==col and col>0 and col<n-1):
+            print("*",end="")
+        else:
+            print(end=" ")
+    print()
+
+
+
+#fixed shape of 5 rows
 for row in range(5):
     for col in range(5):
         if row==0 or col==4 or (row==col and col>0 and col<4):

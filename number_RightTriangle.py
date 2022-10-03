a93964a690ed1722a9e5ed591efb996bde92c7ba
imswaggyboy <75663923+imswaggyboy@users.noreply.github.com>
2022-10-03 15:26:32 +0530

    Printing Numbers in Right Triangle Shape

---
+++ number_RightTriangle.py
@@ -0,0 +1,23 @@
+#  Printing Numbers in Right Triangle Shape
+n = int(input("Enter the number of rows:"))
+for row in range(n,0,-1):
+    for col in range(1,row+1):
+        print(col,end="")
+    print()
+# output = 12345
+#          1234
+#          123
+#          12
+#          1
+
+n = int(input("Enter the number of rows:"))
+for row in range(n,0,-1):
+    for col in range(1,row+1):
+        print(col,end="")
+    print()
+    
+#  output = 55555
+#           4444
+#           333
+#           22
+#           1

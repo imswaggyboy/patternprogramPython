9a5722cd64e40cd57af2dfa0f71e097310450fc9
imswaggyboy <75663923+imswaggyboy@users.noreply.github.com>
2022-10-03 13:12:34 +0530

    Update alphaN_pattern.py

--- alphaN_pattern.py
+++ alphaN_pattern.py
@@ -6,3 +6,11 @@ for row in range(7):
         else:
             print(end=" ")
     print()
+#instead of writing this much logical operation we can use our brain to optimize it 
+for row in range(6):
+    for col in range(6):
+        if col==0 or col==5 or (row==col and col>0 and col<5):
+            print("*",end="")
+        else:
+            print(end=" ")
+    print()
